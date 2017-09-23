57827ad1499058d8fbf9df003e888990645b2301
asoroosh <srafyouni@gmail.com>
2017-09-23 14:06:30 +0100

    BCF>HBCF

--- FastHetBiv/HetBivCalc_fft.m
+++ FastHetBiv/HetBivCalc_fft.m
@@ -41,7 +41,7 @@ I = size(Y,1);
 
 if sum(strcmpi(varargin,'Method'))
    CFm = varargin{find(strcmpi(varargin,'Method'))+1};
-    if strcmpi(CFm,'BCF')
+    if strcmpi(CFm,'BHCF')
         CFmethod=1;
         lagcc=0;
     elseif strcmpi(CFm,'CF0')
@@ -56,7 +56,7 @@ if sum(strcmpi(varargin,'Method'))
            if ~lagcc; lagcc=1; end;
         end
     else
-        error('Unknown BCF methods. Choose between BCF, BC0, BCx');
+        error('Unknown CF methods. Choose between BHCF, BC0, BCx');
     end
 else
    CFmethod=1;
@@ -67,20 +67,20 @@ xAC      = AC_fft(Y,L);
 xAC(:,1) = []; %because we later take care of that little lag0! 
 nLg      = L-1;
 
-wgt     = (nLg-(1:nLg));
-CF     = wgt.*xAC*xAC'; %pfff
-CF     = (L+2*(CF))./L;
+wgt = (nLg-(1:nLg));
+CF  = wgt.*xAC*xAC'; %pfff
+CF  = (L+2*(CF))./L;
 
 if CFmethod==1
-    CF(CF<1)=1; %ensure that there are no node with BCF smaller than 1.
-    EDF=L./CF; 
+    CF(CF<1) = 1; %ensure that there are no node with BCF smaller than 1.
+    EDF = L./CF; 
     return; 
 end
 
 if CFmethod==2
-    CF=CF+corr(Y').^2;
-    CF(CF<1)=1; %ensure that there are no node with BCF smaller than 1.
-    EDF=L./CF; 
+    CF = CF+corr(Y').^2;
+    CF(CF<1) = 1; %ensure that there are no node with BCF smaller than 1.
+    EDF = L./CF; 
 end
 
 if CFmethod==3
@@ -90,9 +90,9 @@ if CFmethod==3
     xC  = bsxfun(@times,xC.^2,reshape(wgt,1,1,numel(wgt))); %here, wgt is multiplied by each plain of IxI of xC!
     xC  = sum(xC,3);
     xC  = xC+triu(xC,1)';
-    CF = CF+xC./L;
+    CF  = CF+xC./L;
     
-    CF(CF<1)=1; %ensure that there are no node with BCF smaller than 1.
+    CF(CF<1) = 1; %ensure that there are no node with BCF smaller than 1.
     
     EDF = L./CF;
 end 
