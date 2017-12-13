f1ba984063827214ad023493910b9c0306e41f84
asoroosh <srafyouni@gmail.com>
2017-12-13 14:19:18 +0000

    twicks

--- Aux/MakeMeCovMat_sqrtm.m
+++ Aux/MakeMeCovMat_sqrtm.m
@@ -1,5 +1,5 @@
 function [sigCmat]=MakeMeCovMat_sqrtm(sigC,ndp)
-
+% this should be removed later! It was only for testing porpusing!
 if size(sigC,1)==numel(sigC); sigC=sigC'; end;
 db0     = [fliplr(sigC) 1 sigC];
 sigC0   = spdiags(ones(ndp,1)*db0,(-numel(sigC):numel(sigC)),ndp,ndp);
