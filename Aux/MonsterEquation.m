5b01fb3ee71684d4b5074475a7aa8be42cb2247a
asoroosh <srafyouni@gmail.com>
2017-12-17 13:19:35 +0000

    Shrinking+Curbing

--- Aux/MonsterEquation.m
+++ Aux/MonsterEquation.m
@@ -4,39 +4,24 @@ if size(ts,1) ~= 2 || size(ts,2) ~= T
     error('PFF!')
 end
 
+%ts  = dtrend(ts);
 ts  = ts./std(ts,[],2); %standardise
-%Corr
+%Corr----------------------------------------------------------------------
 c   = corr(ts');
 rho = c(1,2);
-
-%Curbing Factors-----------------------------------------------------------
-% M    = round(sqrt(T));
-% N    = M; 
-
-N = T-1;
 %Autocorr------------------------------------------------------------------
-ac   = AC_fft(ts,T);
-
+[ac] = AC_fft(ts,T);
 %Tukey Tappering----------------------------------------
-% M             = round(sqrt(T));
-% ac_tmp        = zeros(size(ac));
-% ac_tmp(:,1)   = 1;
-% ac_tmp(:,2:M) = (1+cos([2:M].*pi./M))./2.*ac(:,2:M);
-% ac            = ac_tmp; clear *_tmp;
-% 
-% plot(ac')
-
+%    Mac = T/5; %round(sqrt(T));
+%    ac(1,:) = tukeytaperme(ac(1,:),Mac,1);
+%    ac(2,:) = tukeytaperme(ac(2,:),Mac,1);
+%Shrinkage------------------------------------------------
+ac(1,:) = shrinkme(ac(1,:));
+ac(2,:) = shrinkme(ac(2,:));
 %Curbing------------------------------------------------
-%ac_tmp  = ac;
-%M       = round(T/5);
-%acpvals = zeros(size(ac_tmp));
-%acpvals(:,1:M) = 1;
-%acpvals = ones(size(ac_tmp));
-%ac      = acpvals.*ac_tmp;
-%clear ac_tmp;
-
-%figure; plot(ac')
-
+% Mac = 9;%T/5; %round(sqrt(T));
+% ac  = curbtaperme(ac,Mac);
+%------------------------------------------------
 ac_x = ac(1,1:T-1);
 ac_y = ac(2,1:T-1);
 
@@ -45,86 +30,94 @@ Sigma_y  = toeplitz(ac_y);
 
 %figure; imagesc(Sigma_x)
 %Cross-corr---------------------------------------------------------------- 
- xcf   = crosscorr(ts(1,:),ts(2,:),T-1);
+ [xcf,lags]  = crosscorr(ts(1,:),ts(2,:),T-1);
 
- % These two lines are for curbed versions:
- % ut_CC = fliplr(xcf(1:L)) .*[ones(1,G) zeros(1,M+1-G)];
- % lt_CC = xcf(M+1:end)       .*[ones(1,G) zeros(1,M+1-G)];
- 
  ut_CC = fliplr(xcf(2:T)); %rhos sit hear, so later, we use -1 diag.
  lt_CC = xcf(T:end-1);
  
- %Tukey Tapering the CROSS correlations-------------------------
- %plot(ut_CC); hold on; plot(lt_CC); 
- 
-%  M = round(9*sqrt(T));
-%  
-%  ut_CC_tmp      = zeros(size(ut_CC));
-%  ut_CC_tmp(:,1) = 1;
-%  ut_CC_tmp(2:M) = (1+cos([2:M].*pi./M))./2.*ut_CC(2:M);
-%  
-%  lt_CC_tmp      = zeros(size(lt_CC));
-%  lt_CC_tmp(:,1) = 1;
-%  lt_CC_tmp(2:M) = (1+cos([2:M].*pi./M))./2.*lt_CC(2:M);
-%  
-%  ut_CC = ut_CC_tmp;
-%  lt_CC = lt_CC_tmp;
- 
- %plot(ut_CC,'linewidth',1.3); hold on; plot(lt_CC,'linewidth',1.3); legend({'2:T','T:End','TT 2:T','TT T:End'})
- 
- %Smoothing-------------------------------------------------------
+%Tukey Tapering the CROSS correlations----------------------------   
+%    Mcc = T/5; %;round(sqrt(T));
+%    ut_CC = tukeytaperme(ut_CC,Mcc,rho);
+%    lt_CC = tukeytaperme(lt_CC,Mcc,rho);
+%Shrinkage------------------------------------------------
+ut_CC = shrinkme(ut_CC);
+lt_CC = shrinkme(lt_CC);
+
+plot(ut_CC)
+%Curbing---------------------------------------------------------
+% Mcc   = 9; %T/5; %round(sqrt(T));
+% ut_CC = curbtaperme(ut_CC,Mcc);
+% lt_CC = curbtaperme(lt_CC,Mcc);
+%Smoothing-------------------------------------------------------
  %KrnlSz = 2;
  %Sigma_xy = conv2(Sigma_xy, ones(KrnlSz)/KrnlSz^2,'same');
  %Sigma_xy = eye(T-1).*rho; % no cross-correlation; only 0lag corr
  %figure; imagesc(Sigma_xy) 
 %--------------------------------------------------------------------------
-
-Sigma_xy = triu(toeplitz(ut_CC))+tril(toeplitz(lt_CC),-1); 
-
-% figure; imagesc(Sigma_xy)
+Sigma_xy = (triu(toeplitz(ut_CC))+tril(toeplitz(lt_CC),-1))'; 
+figure; imagesc(Sigma_xy)
+% figure; 
+% subplot(3,2,1); hold on;
+% title('Sigma_{xy}')
+% imagesc(Sigma_xy,[-1 1])
+% disp(['Sigma_{xy}: ' num2str(trace(Sigma_xy))])
+% 
+% subplot(3,2,2); hold on; 
+% title('Sigma_x x Sigma_y')
+% imagesc(Sigma_x * Sigma_y,[-1 1])
+% disp(['Sigma_{x}Sigma_{y}: ' num2str(trace(Sigma_x * Sigma_y)) ]);
+% 
+% subplot(3,2,3); hold on; 
+% title('Sigma_x x Sigma_{xy}')
+% imagesc(Sigma_x * Sigma_xy,[-1 1])
+% disp(['Sigma_{x}Sigma_{xy}: ' num2str(trace(Sigma_x * Sigma_xy)) ]);
+% 
+% subplot(3,2,4); hold on; 
+% title('Sigma_y x Sigma_{xy}')
+% imagesc(Sigma_y * Sigma_xy,[-1 1])
+% disp(['Sigma_{y}Sigma_{xy}: ' num2str(trace(Sigma_y * Sigma_xy))])
+% 
+% subplot(3,2,5); hold on; 
+% title('Sigma x')
+% imagesc(Sigma_x,[-1 1])
+% disp(['Sigma_{x}: ' num2str(trace(Sigma_x))])
+% 
+% subplot(3,2,6); hold on; 
+% title('Sigma y')
+% imagesc(Sigma_y,[-1 1])
+% disp(['Sigma_{y}: ' num2str(trace(Sigma_y))])
 
 CnRe = trace(Sigma_x * Sigma_y); % Clifford and Richardson
 
-% disp('----')
-% disp('-2.*rho.*trace(Sigma_x * Sigma_xy)')
-% -2.*rho     .* trace(Sigma_x * Sigma_xy)
-% trace(Sigma_x * Sigma_xy)
-% disp('-2.*rho.*trace(Sigma_y * Sigma_xy)')
-% -2.*rho     .* trace(Sigma_y * Sigma_xy)
-% trace(Sigma_y * Sigma_xy)
-% disp('----')
-
-ASA=((rho.^2./2).* trace(Sigma_x^2         )...
+ASA=((rho.^2./2).* trace(Sigma_x ^2        )...
     +rho.^2     .* trace(Sigma_xy^2        )...
     -2.*rho     .* trace(Sigma_x * Sigma_xy)...
-    +(rho.^2./2).* trace(Sigma_y^2         )...
+    +(rho.^2./2).* trace(Sigma_y ^2        )...
     -2.*rho     .* trace(Sigma_y * Sigma_xy)...
-                 + CnRe                     ...
-                 + trace(Sigma_xy^2        ))./T^2;          
+    + CnRe...
+    + trace(Sigma_xy^2))./T^2;          
 %--------------------------------------------------------------------------          
-CnR=CnRe./T^2;
+CnR=CnRe./T^2; %just to check how bad the others are doing!
 
 
-% SAS=((rho^2/2)* trace(Sigma_x^2         ) ./v_x.^2   ...
-%     +rho^2    * trace(Sigma_xy^2        ) ./(v_x*v_y)...
-%     -2*rho    * trace(Sigma_x * Sigma_xy) ./(v_x*s_x*s_y)  ...
-%     +(rho^2/2)* trace(Sigma_y^2         ) ./v_y.^2  ...
-%     -2*rho    * trace(Sigma_y * Sigma_xy) ./(v_y*s_y*s_x)  ...
-%               + CRe                       ./(v_x*v_y)...
-%               + trace(Sigma_xy^2        ) ./(v_x*v_y))./T^2;
+function ct_ts=curbtaperme(ts,M)
+  M          = round(M);
+  msk        = zeros(size(ts));
+  msk(:,1:M) = 1;
+  ct_ts      = msk.*ts;
+  
+function srnkd_ts=shrinkme(ts)
+L = numel(ts);
+bnd = (sqrt(2)*erfinv(0.95))./sqrt(L);
+idx = find(abs(ts)>bnd);
+isit       = abs(ts)>bnd & (1:L);
+where2stop = find(isit==0);
+where2stop = where2stop(1);
+srnkd_ts   = curbtaperme(ts,where2stop);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function tt_ts=tukeytaperme(ts,M,intv)
+  if ~exist('intv','var'); intv = 1; warning('Oi!'); end;
+  M          = round(M);
+  tt_ts      = zeros(size(ts));
+  tt_ts(:,1) = intv;
+  tt_ts(2:M) = (1+cos([2:M].*pi./M))./2.*ts(2:M);
