a9400aa4d30074aa886f8f3a699616b30c10a41b
asoroosh <srafyouni@gmail.com>
2018-01-26 10:16:05 +0000

    1-rho2added

--- Aux/PearCorrVarEst.m
+++ Aux/PearCorrVarEst.m
@@ -5,12 +5,14 @@ function [ASAt,Stat]=PearCorrVarEst(ts,T,varargin)
 %   ts: Time series as a 2D matrix. 
 %   T : Number of data-points. Just for the sake of sanity checks
 %
-%
+%   Optionals:
+%   'taper' : uses a tapering method to denoise AC functions
+%    
 %%%OUTPUTS:
 %   ASAt : a 2D matrix of size IxI with diagonal set to zero
-%   
+%   Stat : Contains every other non very important stuff!
 %%%DEPENDECIES:
-%   AC_fft.m : estimates ACF super quick vis FFT
+%   AC_fft.m : estimates ACF super quick via FFT
 %   xC_fft.m : estimates cross corr functions super quick via FFT
 %
 %%%REFERENCES:
@@ -75,7 +77,7 @@ fnnf=mfilename; if ~nargin; help(fnnf); return; end; clear fnnf;
             %disp('TAPER SHRINK')
             
             if M>1
-                error('Not yet mate!')
+                error('Not yet, mate!')
 %                 for in=1:nn    
 %                     ac(in,:)  = ShrinkPeriod(ac(in,:),M);
 %                     for jn=1:nn 
@@ -145,12 +147,12 @@ clear wgtm3 acx_* ac
 %-------------------
 
 %Keep your wit about you!
-% TV = (1-rho.^2).^2./T;
-% if sum(sum(ASAt < TV)) 
-%  idx_ex       = find(ASAt < TV);
-%  ASAt(idx_ex) = TV(idx_ex);
-% end  
-% Stat.TV    = TV;
+TV = (1-rho.^2).^2./T;
+if sum(sum(ASAt < TV)) 
+ idx_ex       = find(ASAt < TV);
+ ASAt(idx_ex) = TV(idx_ex);
+end  
+Stat.TV    = TV;
 
 % diagonal is rubbish;
 ASAt(1:nn+1:end) = 0;
@@ -229,7 +231,7 @@ function srnkd_ts=shrinkme(acs,T)
         srnkd_ts   = curbtaperme(acs,T,where2stop);
     end
 end
-
+%--------------------------------------------------------------------------
 function where2stop = FindBreakPoint(acs,T)
 % this finds the breaking points for shrinking the AC. 
 % Nothing serious, just might help with speed...
@@ -248,7 +250,7 @@ function where2stop = FindBreakPoint(acs,T)
         where2stop = where2stop(1)-1; 
     end;
 end
-
+%--------------------------------------------------------------------------
 function ct_ts=curbtaperme(acs,T,M)
 % Curb the autocorrelations, according to Anderson 1984
 % multi-dimensional, and therefore is fine!
@@ -261,7 +263,7 @@ function ct_ts=curbtaperme(acs,T,M)
     msk(:,1:M) = 1;
     ct_ts      = msk.*acs;
 end
-
+%--------------------------------------------------------------------------
 function tt_ts=tukeytaperme(acs,T,M)
 %performs Single Tukey Tapering for given length of window, M, and initial
 %value, intv. intv should only be used on crosscorrelation matrices.
@@ -280,65 +282,67 @@ function tt_ts=tukeytaperme(acs,T,M)
     %figure; plot(tt_ts); hold on; plot(acs); 
 end
 
-function sY=ShrinkPeriod(Y,WhichPeak)
-%WhichPeak indetified up until how many bumps we should continue (above-CI
-%bumps, of course).
-%NB! This function uses some external functions. 
-%SA, Ox, 2018
-    T = numel(Y);
-    bnd=(sqrt(2)*erfinv(0.95))./sqrt(T);
-    
-    P  = round(InterX([1:T;Y],[1:T;zeros(1,T)]));
-    P  = P(1,:); 
-    P0 = [1 P(1,:)];
-    
-    if WhichPeak>numel(P)
-        warning('There are less peaks than you asked for.')
-        WhichPeak=numel(P);
-    end
-    
-    Idx=zeros(1,WhichPeak);
-    for p=1:WhichPeak; pY = Y(P0(p):P(p)); if any(abs(pY)>bnd); Idx(p)=1; end; end;
-    
-    for i=Idx
-        if ~i; break; end; 
-        Idx(i)=1; 
-    end
-    
-    sY0 = Y(1:max(P(find(Idx))));
-    sY  = zeros(1,T);
-    sY(1:numel(sY0)) = sY0;
-end
-function P = InterX(L1,L2)
-       
-    %...Preliminary stuff
-    x1  = L1(1,:)';  x2 = L2(1,:);
-    y1  = L1(2,:)';  y2 = L2(2,:);
-    dx1 = diff(x1); dy1 = diff(y1);
-    dx2 = diff(x2); dy2 = diff(y2);
-    
-    %...Determine 'signed distances'   
-    S1 = dx1.*y1(1:end-1) - dy1.*x1(1:end-1);
-    S2 = dx2.*y2(1:end-1) - dy2.*x2(1:end-1);
-    
-    C1 = feval(@le,D(bsxfun(@times,dx1,y2)-bsxfun(@times,dy1,x2),S1),0);
-    C2 = feval(@le,D((bsxfun(@times,y1,dx2)-bsxfun(@times,x1,dy2))',S2'),0)';
-
-    %...Obtain the segments where an intersection is expected
-    [i,j] = find(C1 & C2); 
-    if isempty(i),P = zeros(2,0);return; end;
-    
-    %...Transpose and prepare for output
-    i=i'; dx2=dx2'; dy2=dy2'; S2 = S2';
-    L = dy2(j).*dx1(i) - dy1(i).*dx2(j);
-    i = i(L~=0); j=j(L~=0); L=L(L~=0);  %...Avoid divisions by 0
-    
-    %...Solve system of eqs to get the common points
-    P = unique([dx2(j).*S1(i) - dx1(i).*S2(j), ...
-                dy2(j).*S1(i) - dy1(i).*S2(j)]./[L L],'rows')';
-              
-    function u = D(x,y)
-        u = bsxfun(@minus,x(:,1:end-1),y).*bsxfun(@minus,x(:,2:end),y);
-    end
-end
+% function sY=ShrinkPeriod(Y,WhichPeak)
+% %WhichPeak indetified up until how many bumps we should continue (above-CI
+% %bumps, of course).
+% %NB! This function uses pieces from an external package.
+%
+% %SA, Ox, 2018
+%
+%     T = numel(Y);
+%     bnd=(sqrt(2)*erfinv(0.95))./sqrt(T);
+%     
+%     P  = round(InterX([1:T;Y],[1:T;zeros(1,T)]));
+%     P  = P(1,:); 
+%     P0 = [1 P(1,:)];
+%     
+%     if WhichPeak>numel(P)
+%         warning('There are less peaks than you asked for.')
+%         WhichPeak=numel(P);
+%     end
+%     
+%     Idx=zeros(1,WhichPeak);
+%     for p=1:WhichPeak; pY = Y(P0(p):P(p)); if any(abs(pY)>bnd); Idx(p)=1; end; end;
+%     
+%     for i=Idx
+%         if ~i; break; end; 
+%         Idx(i)=1; 
+%     end
+%     
+%     sY0 = Y(1:max(P(find(Idx))));
+%     sY  = zeros(1,T);
+%     sY(1:numel(sY0)) = sY0;
+% end
+% function P = InterX(L1,L2)
+%        
+%     %...Preliminary stuff
+%     x1  = L1(1,:)';  x2 = L2(1,:);
+%     y1  = L1(2,:)';  y2 = L2(2,:);
+%     dx1 = diff(x1); dy1 = diff(y1);
+%     dx2 = diff(x2); dy2 = diff(y2);
+%     
+%     %...Determine 'signed distances'   
+%     S1 = dx1.*y1(1:end-1) - dy1.*x1(1:end-1);
+%     S2 = dx2.*y2(1:end-1) - dy2.*x2(1:end-1);
+%     
+%     C1 = feval(@le,D(bsxfun(@times,dx1,y2)-bsxfun(@times,dy1,x2),S1),0);
+%     C2 = feval(@le,D((bsxfun(@times,y1,dx2)-bsxfun(@times,x1,dy2))',S2'),0)';
+% 
+%     %...Obtain the segments where an intersection is expected
+%     [i,j] = find(C1 & C2); 
+%     if isempty(i),P = zeros(2,0);return; end;
+%     
+%     %...Transpose and prepare for output
+%     i=i'; dx2=dx2'; dy2=dy2'; S2 = S2';
+%     L = dy2(j).*dx1(i) - dy1(i).*dx2(j);
+%     i = i(L~=0); j=j(L~=0); L=L(L~=0);  %...Avoid divisions by 0
+%     
+%     %...Solve system of eqs to get the common points
+%     P = unique([dx2(j).*S1(i) - dx1(i).*S2(j), ...
+%                 dy2(j).*S1(i) - dy1(i).*S2(j)]./[L L],'rows')';
+%               
+%     function u = D(x,y)
+%         u = bsxfun(@minus,x(:,1:end-1),y).*bsxfun(@minus,x(:,2:end),y);
+%     end
+% end
 
