ca7d0804cdbbf1cf0971668d337a37190bff3328
asoroosh <srafyouni@gmail.com>
2018-03-07 11:40:09 +0000

    DamnBuginAC

--- Aux/FoxBCF.m
+++ Aux/FoxBCF.m
@@ -1,4 +1,4 @@
-function [Z,BCF,P] = FoxBCF(Y,T)
+function [Z,BCF,P,BCF_tmp] = FoxBCF(Y,T)
 % The way that Fox and VanDjk estimate the BCF; i.e. Integral [sci] of ACFs 
 % Fox et al 2005 and VanDijek 200X
 %
@@ -27,7 +27,6 @@ function [Z,BCF,P] = FoxBCF(Y,T)
 %   into dynamic, anticorrelated functional networks. Proceedings of the 
 %   National Academy of Sciences of the United States of America, 102(27), 
 %   9673?8. http://doi.org/10.1073/pnas.0504136102
-%
 %_________________________________________________________________________
 % Soroosh Afyouni, University of Oxford, 2018
 % srafyouni@gmail.com
@@ -38,9 +37,9 @@ if size(Y,1)~=T
     Y=Y'; %IxT
 end
 
-ac        = AC_fft(Y,T);
-ac(:,end) = [];
-BCF       = 1+2*sum(ac.^2,2);
-BCF       = mean(BCF);
-Z         = atanh(corr(Y)).*sqrt(T./BCF-3);
-P         = 2 * normcdf(-abs(Z)); 
+ac              = AC_fft(Y,T);
+ac(:,[1 end])   = []; %the last one is shit, the first one is 1!!
+BCF_tmp         = 1+2.*sum(ac.^2,2);
+BCF             = mean(BCF_tmp);
+Z               = atanh(corr(Y)).*sqrt(T./BCF-3);
+P               = 2 * normcdf(-abs(Z)); 
