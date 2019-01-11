b467c72d18e5639498afcfbbc44ab8270b220654
asoroosh <srafyouni@gmail.com>
2019-01-11 15:01:27 +0000

    TinyLittleFixes

--- Aux/AC_fft.m
+++ Aux/AC_fft.m
@@ -1,11 +1,11 @@
-function [xAC,CI,ACOV]=AC_fft(Y,L,varargin)
+function [xAC,CI,ACOV]=AC_fft(Y,T,varargin)
 %[xAC]=AC_fft(Y,T,varargin)
 % Super fast full-lag AC calculation of multi-dimention matrices. The
 % function exploits fft to estimate the autocorrelations. 
 %
 %%%%INPUTS
 %   Y:      A matrix of size IxT comprised of I time series of T length.
-%   L:      Time series length
+%   T:      Time series length
 %   
 %   To get a double sided AC, add 'two-sided' as an input.
 %
@@ -25,11 +25,11 @@ function [xAC,CI,ACOV]=AC_fft(Y,L,varargin)
 fnnf=mfilename; if ~nargin; help(fnnf); return; end; clear fnnf;
 %_________________________________________________________________________
 
-if size(Y,2)~=L
+if size(Y,2)~=T
     Y=Y';
 end
 
-if size(Y,2)~=L
+if size(Y,2)~=T
     error('Use IxT or TxI input form.')
 end
 
@@ -38,23 +38,23 @@ Y = Y-mean(Y,2);
 % if <2016, use Y=Y-repmat(mean(Y,2),1,L) instead.
 
 YNorm   = sum(abs(Y).^2,2);
-nfft    = 2.^nextpow2(2*L-1); %zero-pad the hell out!
+nfft    = 2.^nextpow2(2*T-1); %zero-pad the hell out!
 yfft    = fft(Y,nfft,2); %be careful with the dimensions
 
 clear Y %yeah! that bad!
 
 ACOV = real(ifft(yfft.*conj(yfft),[],2));
 
-ACOV = ACOV(:,1:L);
+ACOV = ACOV(:,1:T);
 
 xAC  = ACOV./YNorm; %normalise the COVs
 
 if sum(strcmpi(varargin,'two-sided')) %two sided is just mirrored, AC func is symmetric
-   xAC  = [xAC(:,end-L+2:end) ; xAC(:,1:L)];
+   xAC  = [xAC(:,end-T+2:end) ; xAC(:,1:T)];
 else
-    xAC  = xAC(:,1:L);
+    xAC  = xAC(:,1:T);
 end
 
-bnd=(sqrt(2)*erfinv(0.95))./sqrt(L); %assumes normality for AC
+bnd=(sqrt(2)*erfinv(0.95))./sqrt(T); %assumes normality for AC
 CI=[-bnd bnd];
 
