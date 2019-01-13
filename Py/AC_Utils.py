7aef9b55ac094da2de3428e949018b0495df2821
asoroosh <srafyouni@gmail.com>
2019-01-13 19:16:20 +0000

    added lots of new modules

--- Py/AC_Utils.py
+++ Py/AC_Utils.py
@@ -7,10 +7,23 @@ Created on Fri Mar 16 16:53:44 2018
 University of Oxford, 2019
 srafyouni@gmail.com
 """
+import numpy as np 
+import math
 
-def AC_fft(Y,T):
-    import numpy as np 
+def autocorr(x, t=1):
+    """ dumb autocorrelation on a 1D array, 
+    almost identical to matlab autocorr()"""
+    x = x.copy()
+    x = x - np.tile(np.mean(x),np.size(x))
+    AC = np.zeros(t)
+    for l in np.arange(t):
+        AC[l] = np.corrcoef(np.array([x[0:len(x)-l], x[l:len(x)]]))[0,1]
+    return AC
+
+def AC_fft(Y,T,copy=True):
     
+    if copy:
+        Y = Y.copy()
     
     if np.shape(Y)[1]!=T:
         print('AC_fft::: Input should be in IxT form, the matrix was transposed.')
@@ -20,13 +33,13 @@ def AC_fft(Y,T):
     mY2 = np.mean(Y,axis=1)
     Y = Y-np.transpose(np.tile(mY2,(T,1)));
     
-    nfft    = nextpow2(2*T-1); #zero-pad the hell out!
-    yfft    = np.fft.fft(Y,n=nfft,axis=1); #be careful with the dimensions
-    ACOV = np.real(np.fft.ifft(yfft*np.conj(yfft),axis=1));
-    ACOV = ACOV[:,0:T-1];
+    nfft  = nextpow2(2*T-1); #zero-pad the hell out!
+    yfft  = np.fft.fft(Y,n=nfft,axis=1); #be careful with the dimensions
+    ACOV  = np.real(np.fft.ifft(yfft*np.conj(yfft),axis=1));
+    ACOV  = ACOV[:,0:T];
     
     Norm = np.sum(np.abs(Y)**2,axis=1);
-    Norm = np.transpose(np.tile(Norm,(T-1,1)));
+    Norm = np.transpose(np.tile(Norm,(T,1)));
     xAC  = ACOV/Norm; #normalise the COVs   
     
     bnd  = (np.sqrt(2)*1.3859)/np.sqrt(T); #assumes normality for AC
@@ -34,14 +47,16 @@ def AC_fft(Y,T):
     
     return xAC,CI
 
-
-def xC_fft(Y,T, mxL = [], **kwargs):
-    import numpy as np
+def xC_fft(Y,T, mxL = [], copy=True):
 
 # ***********************************
 # This should be checked! There shouldn't be any complex numbers!!
 #__main__:74: ComplexWarning: Casting complex values to real discards the imaginary part
+# This is because Python, in contrast to Matlab, produce highly prcise imaginary parts 
+# by defualt, when you wanna do ifft, just use np.real()    
 # ***********************************
+    if copy:
+        Y = Y.copy()
     
     if np.shape(Y)[1]!=T:
         print('xC_fft::: Input should be in IxT form, the matrix was transposed.')
@@ -52,32 +67,33 @@ def xC_fft(Y,T, mxL = [], **kwargs):
     
     I = np.shape(Y)[0]
     
-    print("AC_fft::: Demean along T")
+    print("xC_fft::: Demean along T")
     mY2 = np.mean(Y,axis=1)
     Y   = Y-np.transpose(np.tile(mY2,(T,1)));    
     
     nfft    = nextpow2(2*T-1); #zero-pad the hell out!
     yfft    = np.fft.fft(Y,n=nfft,axis=1); #be careful with the dimensions    
-
+    
     mxLcc = (mxL-1)*2+1
-    xC    = np.empty([I,I,mxLcc]);
+    xC    = np.zeros([I,I,mxLcc]);
         
-    XX = np.where(np.triu(np.ones([I,I]),1))[0]
-    YY = np.where(np.triu(np.ones([I,I]),1))[1]
-        
-    for i in np.arange(0,np.size(XX)): #loop around edges
-        
-        xC0 = np.fft.ifft(yfft[XX[i],:]*np.conj(yfft[YY[i],:]))
-        xC0 = np.concatenate((xC0[-mxL+1:],xC0[0:mxL]))                
-        xC0 = np.fliplr([xC0])[0]        
-        Norm = np.sqrt(np.sum(np.abs(Y[XX[i],:])**2)*np.sum(np.abs(Y[YY[i],:])**2))     
+    XX = np.triu_indices(I,1)[0]
+    YY = np.triu_indices(I,1)[1]
+            
+    for i in np.arange(np.size(XX)): #loop around edges.
+               
+        xC0 = np.fft.ifft(yfft[XX[i],:]*np.conj(yfft[YY[i],:]),axis=0)
+        xC0 = np.real(xC0)
+        xC0 = np.concatenate((xC0[-mxL+1:None],xC0[0:mxL]))  
                 
-        xC0 = xC0/Norm;        
-        xC[XX[i],YY[i],:]   = xC0
+        xC0 = np.fliplr([xC0])[0]
+        Norm = np.sqrt(np.sum(np.abs(Y[XX[i],:])**2)*np.sum(np.abs(Y[YY[i],:])**2))     
+        
+        xC0 = xC0/Norm;              
+        xC[XX[i],YY[i],:] = xC0
         del xC0
     
-    xC = xC + np.transpose(xC,(1,0,2));
-    
+    xC   = xC + np.transpose(xC,(1,0,2));    
     lidx = np.arange(-(mxL-1),mxL);
     
     return xC,lidx
@@ -89,9 +105,15 @@ def nextpow2(x):
     often useful for finding the nearest power of two sequence
     length for FFT operations.
     """
-    import math
     return 1 if x == 0 else 2**math.ceil(math.log2(x))
 
+
+def ACL(ts,T):
+    """
+    Calculates Autocorrelation Length of time series ts
+    SA, Ox, 2019
+    """
+    return(np.sum(AC_fft(ts,T)**2,axis=1))
 #import scipy.io
 #import numpy as np
 #V = '/Users/sorooshafyouni/Home/BCF/BCFAnal/FC/100HCPTimeSeries/Yeo/HCP_FPP_124422_OnlyMTS.mat'
